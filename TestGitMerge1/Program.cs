b77d6ad6271881f8dc33cb54d9280a1d022a8478
Athar Imam <mohammadatharimam@gmail.com>
2020-07-26 17:30:11 +0530

    f4

--- TestGitMerge1/Program.cs
+++ TestGitMerge1/Program.cs
@@ -14,6 +14,7 @@ namespace TestGitMerge1
             Console.WriteLine("f3");
             Console.WriteLine("m4");
             Console.WriteLine("m5");
+            Console.WriteLine("f4");
         }
     }
 }
