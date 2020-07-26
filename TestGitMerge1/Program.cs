550d713da4a627f8ba33d4a5863a4b3b4f5d0212
Athar Imam <mohammadatharimam@gmail.com>
2020-07-26 17:17:06 +0530

    Add project files.

---
+++ TestGitMerge1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGitMerge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
